6819db698f18501cdce5d3a2ffd45af1f31eff9f
David <david@rosenblum.net>
2019-10-16 21:16:42 -0400

    Database comments

--- src/server/database/DBAccounts.ts
+++ src/server/database/DBAccounts.ts
@@ -15,30 +15,56 @@ export interface SaltSchema{
     salt:string;
 }
 
+/**
+ * Creates a md5 hash of the given input string
+ * @param input value to hash
+ * @returns the md5 hash string
+ */
 const md5Hash = (input:string):string => {
     return createHash("md5").update(input).digest("hex");
 };
 
+/**
+ * Creates a new account in the database
+ * @param username  new account name
+ * @param password  new account password
+ */
 export const insertAccount = (username:string, password:string):Promise<{}> => {
     return new Promise((resolve, reject) => {
         const {db} = GameDatabase.instance;
 
+        // generate random salt
         const salt:string = randomBytes(8).toString("hex");
+        // hash the password
         const pwHash:string = md5Hash(password + salt);
 
+        // create the account document
         const accountDoc:AccountSchema = {
-            username, password: pwHash, accessLevel: 1, enabled: true, dateCreated: Timestamp.fromInt(Date.now())
+            username,
+            password:       pwHash,
+            accessLevel:    1,
+            enabled:        true,
+            dateCreated:    Timestamp.fromInt(Date.now())
         };
 
+        // insert the account document into the database
         db.collection<AccountSchema>("accounts").insertOne(accountDoc, (err, doc) => {
+            // error
             if(err){
                 reject(err);
                 return;
             }
 
+            // get database-generated accountId
             const accountId:string = doc.insertedId.toHexString();
-            const saltDoc:SaltSchema = {accountId, salt};
+            
+            // create salt document
+            const saltDoc:SaltSchema = {
+                accountId,
+                salt
+            };
 
+            // insert salt into the database
             db.collection<SaltSchema>("salts").insertOne(saltDoc, err => {
                 err ? reject(err) : resolve();
             });
@@ -47,53 +73,75 @@ export const insertAccount = (username:string, password:string):Promise<{}> => {
 
 };
 
+/**
+ * Selects a specific account from the database
+ * @param username  account username
+ * @param password  account password
+ * @returns find account promise
+ */
 export const findAccount = (username:string, password:string):Promise<AccountSchema> => {
     return new Promise((resolve, reject) => {
         const {db} = GameDatabase.instance;
 
+        // find the account by username
         db.collection<AccountSchema>("accounts").findOne({username}, (err, accountDoc) => {
+            // error
             if(err){
                 reject(err);
                 return;
             }
 
+            // no result (wrong username)
             if(!accountDoc){
                 reject(new Error("Wrong username or password."));
                 return;
             }
 
+            // get accountId from the results document (typescript unaware, thus the 'any' cast)
             const accountId:string = (accountDoc as any)._id.toString("hex");
             
+            // find the account salt
             db.collection<SaltSchema>("salts").findOne({accountId}, (err, saltDoc) => {
+                // error
                 if(err){
                     reject(err);
                     return;
                 }
 
+                // no salt document (shouldn't happen)
                 if(!saltDoc){
                     console.log(`ERR: salt document missing for accountId=${accountId}.`);
                     reject(new Error("Server error."));
                     return;
                 }
 
+                // hash the input password (as the correct password stored is a hash)
                 const pwHash:string = md5Hash(password + saltDoc.salt);
 
+                // verify hashed input password is equal to stored password hash
                 (pwHash === accountDoc.password) ? resolve(accountDoc) : reject(new Error("Wrong username or password."));
             });
         });
     });
 };
 
+/**
+ * Creates the accounts collection
+ * @returns create collection promise
+ */
 export const createAccountsCollection = ():Promise<{}> => {
     return new Promise((resolve, reject) => {
         const {db} = GameDatabase.instance;
 
+        // create the accounts collection
         db.createCollection("accounts", (err, col) => {
             if(err){
+                // error
                 reject(err);
                 return;
             }
 
+            // create username index
             col.createIndex("username", {unique: true}, err => {
                 err ? reject(err) : resolve();
             });
@@ -101,16 +149,22 @@ export const createAccountsCollection = ():Promise<{}> => {
     });
 };
 
+/**
+ * Creates the salt collection
+ * @returns create collection promise
+ */
 export const createSaltsCollection = ():Promise<{}> => {
     return new Promise((resolve, reject) => {
         const {db} = GameDatabase.instance;
 
+        // create the salts collection
         db.createCollection("salts", err => {
             err ? reject(err) : resolve();
         });
     });
 };
 
+// export functions in a single object
 export const DBAccounts = {
     insertAccount, findAccount, createAccountsCollection, createSaltsCollection
 };
--- src/server/database/DBPlayers.ts
+++ src/server/database/DBPlayers.ts
@@ -1,3 +1,4 @@
+import { UpdateQuery } from "mongodb";
 import { GameDatabase } from "./GameDatabase";
 
 export interface PlayerSchema{
@@ -21,12 +22,19 @@ export interface PlayerListSchema{
     level:number;
 }
 
+/**
+ * Finds all the player summaries for an account
+ * @param accountId account to find all players for
+ * @returns find player summaries promise
+ */
 export const findPlayerList = (accountId:string):Promise<PlayerListSchema[]> => {
     return new Promise((resolve, reject) => {
         const {db} = GameDatabase.instance;
 
+        // find all players matching the account id
         db.collection<PlayerSchema>("players").find({accountId}).toArray()
             .then(res => {
+                // no error, remove sensitive information from each results element
                 const list:PlayerListSchema[] = res.map(doc => {
                     const {name, level, archetype} = doc;
                     return {archetype, level, name};
@@ -37,10 +45,18 @@ export const findPlayerList = (accountId:string):Promise<PlayerListSchema[]> => 
     });
 };
 
+/**
+ * Inserts a new player into the database
+ * @param accountId new player's associated accountId
+ * @param name      new player's name
+ * @param archetype new player's archetype (id not name)
+ * @returns collection insert promise
+ */
 export const insertPlayer = (accountId:string, name:string, archetype:number):Promise<{}> => {
     return new Promise((resolve, reject) => {
         const {db} = GameDatabase.instance;
 
+        // create the player document
         const playerDoc:PlayerSchema = {
             accountId,
             name,
@@ -50,56 +66,86 @@ export const insertPlayer = (accountId:string, name:string, archetype:number):Pr
             merits: 0
         };
 
-        db.collection<PlayerSchema>("players").insertOne(playerDoc, (err, res) => {
+        // insert document into the database
+        db.collection<PlayerSchema>("players").insertOne(playerDoc, err => {
             err ? reject(err) : resolve();
         });
     }); 
 };
 
+/**
+ * Selects a player from the database
+ * @param accountId player account id
+ * @param name      player name
+ * @returns collection find promise 
+ */
 export const findPlayer = (accountId:string, name:string):Promise<PlayerSchema> => {
     return new Promise((resolve, reject) => {
         const {db} = GameDatabase.instance;
 
+        // find in the database by accountId and name
         db.collection<PlayerSchema>("players").findOne({accountId, name}, (err, playerDoc) => {
             err ? reject(err) : resolve(playerDoc);
         });
     });
 };
 
+/**
+ * Deletes a player from the database
+ * @param accountId player account id
+ * @param name      player name
+ * @returns collection delete promise 
+ */
 export const deletePlayer = (accountId:string, name:string):Promise<boolean> => {
     return new Promise((resolve, reject) => {
         const {db} = GameDatabase.instance;
 
+        // delete from the database... find by accountId and name
         db.collection<PlayerSchema>("players").deleteOne({accountId, name}, (err, res) => {
             err ? reject(err) : resolve(res.deletedCount > 0);
         });
     });
 };
 
+/**
+ * Updates the player in the database
+ * @param name      player name
+ * @param update    update objece
+ * @returns collection update promise 
+ */
 export const updatePlayer = (name:string, update:PlayerUpdateSchema):Promise<{}> => {
     return new Promise((resolve, reject) => {
         const {db} = GameDatabase.instance;
 
-        const mongoUpdate = {
+        // update query
+        const mongoUpdate:UpdateQuery<PlayerSchema> = {
             $set: update
         };
 
+        // update the database... find by name and apply update
         db.collection<PlayerSchema>("players").updateOne({name}, mongoUpdate, err => {
             err ? reject(err) : resolve();
         });
     });
 }
 
+/**
+ * Creates the player collection
+ * @returns collection create promise 
+ */
 export const createPlayerCollection = ():Promise<{}> => {
     return new Promise((resolve, reject) => {
         const {db} = GameDatabase.instance;
 
+        // create the players collection
         db.createCollection("players", (err, col) => {
             if(err){
+                // error
                 reject(err);
                 return;
             }
 
+            // create indexes 
             Promise.all([
                 col.createIndex("name", {unique: true}),
                 col.createIndex("accountId")
@@ -110,6 +156,7 @@ export const createPlayerCollection = ():Promise<{}> => {
     });
 };
 
+// export functions in an single object
 export const DBPlayers = {
     findPlayerList, findPlayer, insertPlayer, deletePlayer, updatePlayer, createPlayerCollection
 };
