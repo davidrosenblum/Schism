a724b7213d6c924c8bd21f85f6afd9ba496dfe0c
David <david@rosenblum.net>
2019-10-19 00:20:42 -0400

    More abilities & ability animation

--- src/server/abilities/UndeadAbilities.ts
+++ src/server/abilities/UndeadAbilities.ts
@@ -1,6 +1,56 @@
 import { AbilityConfig, AbilityTargets, AbilityRange } from "./Ability";
 import { damageOnce, damageOverTime } from "./AbilityPresets";
 
+export const Ghoul1:AbilityConfig = {
+    name:           "Claw",
+    internalName:   "ghoul1",
+    description:    "",
+    manaCost:       2,
+    targets:        AbilityTargets.ENEMIES_ONLY,
+    affects:        AbilityTargets.ENEMIES_ONLY,
+    range:          AbilityRange.NEAR,
+    maxTargets:     1,
+    recharge:       3,
+    affect: (caster, target, relationship) => {
+        if(target.rollDodge())
+            return false;
+
+        damageOnce({
+            caster,
+            target,
+            baseDamage: 2,
+            critMult: 1.10
+        });
+
+        return true;
+    }
+};
+
+export const GraveKnight1:AbilityConfig = {
+    name:           "Stab",
+    internalName:   "graveknight1",
+    description:    "",
+    manaCost:       3,
+    targets:        AbilityTargets.ENEMIES_ONLY,
+    affects:        AbilityTargets.ENEMIES_ONLY,
+    range:          AbilityRange.NEAR,
+    maxTargets:     1,
+    recharge:       3,
+    affect: (caster, target, relationship) => {
+        if(target.rollDodge())
+            return false;
+
+        damageOnce({
+            caster,
+            target,
+            baseDamage: 4,
+            critMult: 1.25
+        });
+
+        return true;
+    }
+};
+
 export const Lich1:AbilityConfig = {
     name:           "Shadow Bolt",
     internalName:   "lich1",
