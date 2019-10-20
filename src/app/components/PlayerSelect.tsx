7695dcbb6adc8c22f764180b64423f6e9796f217
David <david@rosenblum.net>
2019-10-19 23:45:13 -0400

    New player select UI & map table, menu padding increased

--- src/app/components/PlayerSelect.tsx
+++ src/app/components/PlayerSelect.tsx
@@ -1,11 +1,9 @@
 import * as React from "react";
 import { Button, MenuContainer } from "./core";
 import { store } from "../Client";
-import { showPlayerCreate } from "../actions/MenuActions";
-import { Archetypes } from "../data/ArchetypeData";
+import { PlayerSelectFigure } from "./PlayerSelectFigure";
 import { requestLogout } from "../requests/AccountRequests";
-import { requestPlayerSelect, requestPlayerList, requestPlayerDelete } from "../requests/PlayerListRequests";
-import "./PlayerSelect.css";
+import { requestPlayerList } from "../requests/PlayerListRequests";
 
 export const PlayerSelect = () => {
     React.useEffect(() => {
@@ -25,86 +23,32 @@ export const PlayerSelect = () => {
     };
 
     const onLogout = () => {
-        if(!checkDisabled()){
+        if(!checkDisabled())
             requestLogout();
-        }
-    };
-
-    const onCreate = () => {
-        if(!checkDisabled()){
-            store.dispatch(showPlayerCreate());
-        }
-    };
-
-    const selectPlayer = (name:string) => {
-        if(!checkDisabled()){
-            requestPlayerSelect(name);
-        }
-    };
-
-    const deletePlayer = (name:string) => {
-        if(!checkDisabled()){
-            requestPlayerDelete(name);
-        }
     };
 
     const {
         pendingList, list
     } = store.getState().playerList;
 
-    if(pendingList || !list){
-        return <div>Loading...</div>;
-    }
-
+    const loading:boolean = (pendingList || !list);
     const disabled:boolean = checkDisabled();
-
-    const cells = list.map((val, i) => {
-        const {name, level, archetype} = val;
-
-        return (
-            <figure key={i} className="player-container">
-                <div>
-                    {name}
-                </div>
-                <div>
-                    Level {level} &nbsp;
-                    {Archetypes[archetype] ? Archetypes[archetype].archetype : "Unknown Archetype?"}
-                </div>
-                <br/>
-                <div>
-                    <Button type="button" disabled={disabled} onClick={() => selectPlayer(name)}>
-                        Select
-                    </Button>
-                    &nbsp;
-                    <Button type="button" disabled={disabled} onClick={() => deletePlayer(name)}>
-                        Delete
-                    </Button>
-                </div>
-            </figure>
-        );
-    });
-
-    while(cells.length < 6){
-        cells.push(
-            <figure key={cells.length} className="player-container">
-                <div>
-                    Empty Slot
-                </div>
-                <br/>
-                <div>
-                    <Button type="button" disabled={disabled} onClick={onCreate}>
-                        Create
-                    </Button>
-                </div>
-            </figure>
-        )
-    }
 
     return (
         <MenuContainer>
-            <div>
-                {cells}
-            </div>
+            {
+                loading ? (
+                    <div className="text-center">
+                        Loading...
+                    </div>
+                ) : (
+                    <PlayerSelectFigure
+                        disabled={disabled}
+                        list={list}
+                    />
+                )
+            }
+            <br/>
             <div className="text-center">
                 <Button type="button" disabled={disabled} onClick={onLogout}>
                     Logout
